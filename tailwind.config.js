de1f090b3c4bb8afda26041b0e7e85bc78a81a8f
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-07-25 20:45:22 +0600

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"],
+    theme: {
+      extend: {
+        colors: {
+          'custom-color': '#fff',
+        }
+      },
+    },
+    plugins: [],
+  }
